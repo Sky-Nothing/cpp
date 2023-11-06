a616c5ceb4d7b4d1e5c44eeb51a2eb8b20cc48f2
天无云 <tianwuyun@126.com>
2023-11-06 17:47:58 +0800

    Sky

---
+++ chapter1/02_Namespaces/namespaces.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "namespaces.h"
+void mycode::foo()
+{
+    std::cout << "这是namespace下的foo函数" << std::endl;
+}
---
+++ chapter1/02_Namespaces/usingnamespace.cpp
@@ -0,0 +1,7 @@
+#include "namespaces.h"
+int main(int argc, char const *argv[])
+{
+    using namespace mycode;
+    foo();
+    return 0;
+}
